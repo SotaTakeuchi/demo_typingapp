5d65d0bf4ad6f0623fd620ae74f0ba3e59b6d428
Naoki Itabashi <s1f101901027@iniad.org>
2020-01-10 14:15:31 +0900

    a

--- itabashi/views.py
+++ itabashi/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+#from itabashi.models import ()
 
 # Create your views here.
+def start(request):
+    """トップ画面"""
+    return render(request,'template\typies/start.html')

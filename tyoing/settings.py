e37237e2891705ffa2bdba85575a1002121f0c12
Naoki Itabashi <s1f101901027@iniad.org>
2020-01-20 02:10:53 +0900

    板橋
    頑張った

--- tyoing/settings.py
+++ tyoing/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'typing_app.apps.TypingAppConfig',
+    
 
 
 
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'tyoing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
